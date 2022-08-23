e043dafd3e41a35e5f21e646e90dc3668273ae5f
Minsoo <gkm19809@gmail.com>
2022-08-23 07:33:19 +0000

    fix typo

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+"""
+Main entry script for pygame-web
+"""
 import asyncio
 import pygame
 
 async def main():
-  # initialize and set variables.
-  while True:
-    # update loop
-    await ayncio.sleep(0)
+    """Main function of the game.\nIt\n"""
+    # initialize and set variables.
+    while True:
+        # update loop
+        await asyncio.sleep(0)
 
 asyncio.run(main())
